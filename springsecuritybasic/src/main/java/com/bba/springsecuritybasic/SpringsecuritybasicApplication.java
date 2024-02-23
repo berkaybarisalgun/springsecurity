edecb6a90d8c3fd848f46ed1082cb25b7af55a46
Baris Algun <bbarisalgun@gmail.com>
2024-02-23 17:02:48 +0300

    starters

--- springsecuritybasic/src/main/java/com/bba/springsecuritybasic/SpringsecuritybasicApplication.java
+++ springsecuritybasic/src/main/java/com/bba/springsecuritybasic/SpringsecuritybasicApplication.java
@@ -2,8 +2,11 @@ package com.bba.springsecuritybasic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
+@ComponentScan("com.bba.springsecuritybasic.controller")
 public class SpringsecuritybasicApplication {
 
 	public static void main(String[] args) {
